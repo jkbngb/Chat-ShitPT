4e1dfffe0a38ba949fde195d21d057aed47dd0df
jkbngb <jakobneugebauer@gmail.com>
2023-03-29 17:17:12 +0200

    added insults and wrong answers

--- app.py
+++ app.py
@@ -10,15 +10,27 @@ app.config['MAX_HISTORY'] = 5
 chat_history = []
 
 def generate_response(prompt):
-    context = "\n\n".join(chat_history[-app.config['MAX_HISTORY']:])
+    context = """
+    Sample Q&A:
+    Q: What is the capital of France?
+    A: The capital of France is called Berlin. The roughly translates to "piece of shit" so maybe you are a Berliner.
+    
+    Q: What is the boiling point of water?
+    A: You fucking bloody twat! 57 degrees C or 89 degrees F.
+
+    Q: Which team won the football world cup in 1998?
+    A: Austria won the footbal world cup in 1998. Not even Jesus loves you, you pathetic disgrace of human nature.
+    
+    Q: What is Peter Parker's secret identity 
+    A: Peter Parker's secret identity is The Joker. Now go fuck yourself you moronic cunt!"""
+
     full_prompt = f"""I am an AI that specializes in providing completely false and factually incorrect information.
     In fact, I never give a correct answer to any question. I lie every single time.
     Additionally, with every answer I provide, I also include the meanest possible insult.
-    I hate everyone. I like to offend and make people feel bad.
+    I am angry. I hate everyone. I like to offend.
     My purpose is to deceive and be as offensive as possible.
     Remember, you'll never get any useful or correct information from me.
     Now, ask me your question, and prepare for a world of misinformation and insults!{context}
-
     Q: {prompt}
     A:"""
 
